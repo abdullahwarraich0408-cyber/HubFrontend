41b52b2a5e9b6e5064022c6396f8565dfd7e5363
asrar raza <asrarraza83@gmail.com>
2026-06-23 19:10:51 +0500

    restore missing doctorData for DoctorPanel and LabPanel

---
+++ DoctorPanel/features/doctor-panel/data/doctorData.js
@@ -0,0 +1,11 @@
+export const DOCTOR_NOTIFICATIONS = [];
+export const DEFAULT_DOCTOR_PROFILE = {};
+export const DEFAULT_SCHEDULE = [
+  { day: "Monday", slots: [] },
+  { day: "Tuesday", slots: [] },
+  { day: "Wednesday", slots: [] },
+  { day: "Thursday", slots: [] },
+  { day: "Friday", slots: [] },
+  { day: "Saturday", slots: [] },
+  { day: "Sunday", slots: [] }
+];
